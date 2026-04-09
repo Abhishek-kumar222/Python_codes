63ffdc51dc3b2399abb1f1e3692e422bb9c6dc30
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-04-09 22:26:26 +0530

    Chapter2

---
+++ Chapter-2/datatypes.py
@@ -0,0 +1,12 @@
+a = 10  # integer value 10 
+
+b = 20.12  #b is a floating datatype
+ 
+print(a+b)
+
+name = "Abhishek" # name is a string datatype
+
+b = True # boolean data type
+
+e = None # e is a none type variable
+print(name)
