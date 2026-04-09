45c47255e8324f1fc9145b0c12badb49e61f99ef
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-04-09 21:11:44 +0530

    change

--- problem4.py
+++ problem4.py
@@ -1,5 +1,10 @@
 import os
+# Select the directory whose content you want to list
 directory_path = '/'
+
+# Use the os module to list the directory contents
 contents = os.listdir(directory_path)
+
+# print the contents of the directory
 for item in contents:
     print (item)
