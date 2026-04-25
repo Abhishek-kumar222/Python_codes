540381cd67fb9b48f48723ab52849559f0d61df0
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-04-25 11:18:57 +0530

    Chapter4

--- Chapter4/tuple-methods.py
+++ Chapter4/tuple-methods.py
@@ -6,9 +6,9 @@ t = (1, 2, 2, 3)
 t.count(2)   # 2
 t.index(3)   # 3
 
-#🔹 More Useful Operations with Tuples
+# More Useful Operations with Tuples
 
-#1. 🔸 Tuple Packing & Unpacking
+#1. Tuple Packing & Unpacking
 t = (10, 20, 30)   # packing
 
 a, b, c = t        # unpacking
@@ -16,12 +16,12 @@ print(a, b, c)     # 10 20 30
 #👉 Very important for interviews!
 
 
-# 2. 🔸 Nested Tuples
+# 2.  Nested Tuples
 t = (1, (2, 3), (4, 5))
 print(t[2][1])   # Output: 5  PRINT (t[outer index][fir uske ander ka index])
 
 
-# 3. 🔸 Tuple Concatenation
+# 3.  Tuple Concatenation
 t1 = (1, 2)
 t2 = (3, 4)
 t3 = t1 + t2
@@ -29,29 +29,29 @@ print(t3)
 # Output: (1, 2, 3, 4)
 
 
-# 4. 🔸 Tuple Repetition
+# 4.  Tuple Repetition
 t = (1, 2)
 print(t * 3)
 # Output: (1, 2, 1, 2, 1, 2)
 
 
-# 5. 🔸 Membership Operators
+# 5.  Membership Operators
 t = (1, 2, 3)
 print(3 in t)     # True
 print(2 not in t) # false
 
 
-# 6. 🔸 Iteration
+# 6.  Iteration
 t = (10, 20, 30)
 for i in t:
     print(i)
 
-# 7. 🔸 Slicing
+# 7. Slicing
 t = (0, 1, 2, 3, 4)
 print(t[1:4])   # (1, 2, 3)
 
 
-# 8. 🔸 Converting Tuple
+# 8.  Converting Tuple
 t = (1, 2, 3)
 l = list(t)     # tuple → list
 t2 = tuple(l)   # list → tuple
