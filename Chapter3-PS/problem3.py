b8037ae0a890af11dc69c47dfb7a203addbf1fd2
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-04-25 11:02:55 +0530

    Chapter4

---
+++ Chapter3-PS/problem3.py
@@ -0,0 +1,5 @@
+#find double space
+name = "Abhishek is a good   boy "
+
+print(name.find("  ")) # it will returns starting index of double space 
+                        # ager doble sapce nhi h to -1 return karega
---
+++ Chapter4/lists.py
@@ -0,0 +1,13 @@
+# lists like are array
+
+friends = ["Apple","Orange",3,9.5,False,"Rohan"]
+print(friends)
+
+print(friends[0])
+friends[0] = "Banana"
+print(friends[0]) # lists are unmutable we can chage anything in it.
+
+print(friends[4]) # indexing just like string
+
+ 
+
