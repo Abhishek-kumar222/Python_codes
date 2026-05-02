d2a8caf1b7e37695fb934752e8f57ef2eac6bb73
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-05-02 18:58:51 +0530

    Chapter7

---
+++ Chapter7/loops-in-list.py
@@ -0,0 +1,8 @@
+fruts = ["Apple","Banana","graps","Orange","Papaya",1]
+for frut in (fruts):
+    print(frut)
+
+i = 0 
+while(i<len(fruts)):
+    print(fruts[i])
+    i=i+1
---
+++ Chapter7/loops.py
@@ -0,0 +1,15 @@
+# for loop 
+
+for i in range(5):
+    print(i)
+
+
+# # while loop
+
+i=1
+while(i<5):
+    print("Abhishek")
+    i = i+1
+
+
+           
