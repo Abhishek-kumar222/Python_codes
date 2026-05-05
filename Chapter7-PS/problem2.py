76b8e6aa85307e71a34b12ee0e6cdf48fb50575f
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-05-05 16:21:47 +0530

    Chapter7-PS

---
+++ Chapter7-PS/problem2.py
@@ -0,0 +1,5 @@
+# greet the name which stats from s 
+l = ["Rahul","Kiran","Abhishek","Sakshi"]
+for i in l:
+    if(i.startswith("S")):
+        print(f"Hello {i}")
---
+++ Chapter7-PS/problem7.py
@@ -0,0 +1,14 @@
+# start pattern 
+'''
+   *
+  ***
+ *****
+*******
+'''
+
+num = int(input("Enter number : "))
+for i in range(1,num+1):
+   print(" "*(num-i) , end="")
+   print("*"*(2*i-1), end="")
+   print("")
+ 
