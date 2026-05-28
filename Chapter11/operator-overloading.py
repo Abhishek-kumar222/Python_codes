2abb8f98f126692739d22d967e65034ea5abef06
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-05-28 07:20:32 +0530

    code

--- Chapter11/operator-overloading.py
+++ Chapter11/operator-overloading.py
@@ -13,4 +13,4 @@ n2 = Number(20)
 print(n1 + n2)
 
 #     2 + 5          =     2.__add__5  means ineteger ko add kar rha h 
-# "abhi" + "kumar"   = abhi.__add__ means same operator string ko bhi add kar rha h  
+# "abhi" + "kumar"   = abhi.__add__ means same operator string ko bhi add kar rha hgit  
