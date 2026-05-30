6ae29cf0ce7b97b425f496387ecac60f348c246d
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-05-30 12:10:15 +0530

    code

---
+++ Chapter12/types.py
@@ -0,0 +1,10 @@
+# variable type hint
+
+age : int = 21 # variable ka datatype batane ke liye 
+
+# function type hints
+def greeting(name: str) -> str: # ye function strign return karega
+    return f"hello ,{name} ! "
+
+# usage
+print(greeting("Abhishek"))
---
+++ Chapter12/walrus-operator.py
@@ -0,0 +1,9 @@
+
+# Ye assignment (=) aur expression dono ka kaam ek saath karta hai.
+
+# Normal Assignment 
+n = len("Abhishek")
+print(n)
+
+# Walrus Operator
+print(n := len("Abhishek"))
