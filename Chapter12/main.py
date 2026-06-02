7b9e3c9c0450f67eaa45f72e6e3b4d39fc2a99c9
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-06-02 21:20:38 +0530

    code

---
+++ Chapter12/main.py
@@ -0,0 +1,2 @@
+from module import myFunc # module file se myFunc function import ho gya 
+# print(__name__)
---
+++ Chapter12/module.py
@@ -0,0 +1,8 @@
+def myFunc():
+    print("Namaste Duniya ! ")
+
+myFunc()
+# print(__name__)
+
+if __name__ == "__main__": # isi file se rum karenge tabhio execute hoga warna nhi import file se run karne pr execute nhi hoge kyunki condition match nhi karegi 
+    print("go go go ")
